7d3afb7b40435d4d9cd0e904b989a812bf1c2ce7
Patrick Davey <ghent12@gmail.com>
2022-08-27 10:40:04 -0500

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,28 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(targetArray) {
+  let newArray = []
+    , removalFlag = false;
+  if (Array.isArray(targetArray)) {
+    for (i = 0; i < targetArray.length; i++) {
+      
+      for (j = 1; j < arguments.length; j++) {
+        
+        if (targetArray[i] === arguments[j]) {
+          removalFlag = true;
+        }
+      }
+      if (removalFlag === true) {
+        // Do nothing
+        removalFlag = false;
+      } else {
+        newArray.push(targetArray[i]);
+        removalFlag = false;
+      }
+    }
+    console.log(arguments);
+    return newArray;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
