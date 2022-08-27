cdd558641cf1f3fc25a684f3f43fd1d16c393a66
Patrick Davey <ghent12@gmail.com>
2022-08-26 20:07:46 -0500

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+  let stringLength = inputString.length
+    , holdingArray = [];
 
+  if (typeof inputString == "string") {
+    for (i = 0; i < stringLength; i++) {
+      holdingArray.unshift(inputString[i]);
+    }
+    return (holdingArray.join('').toString());
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
