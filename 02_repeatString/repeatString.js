58d0f264bd382edac6630f944ee373505a9fc795
Patrick Davey <ghent12@gmail.com>
2022-08-26 19:51:25 -0500

    Complete exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(targetString, repetitionNumber) {
+  let repeatedString = targetString;
+  for (i = 1; i < repetitionNumber; ++i) {
+    repeatedString = repeatedString + targetString;
+  }
+  if (repetitionNumber < 0) {
+    return "ERROR";
+  } else if (repetitionNumber == 0 || targetString == "") {    
+    return "";
+  } else {
+    return repeatedString;
+  }
 };
 
 // Do not edit below this line
