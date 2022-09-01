08971713a513872c7c9cd60bc69f7ec30f2a9587
Patrick Davey <ghent12@gmail.com>
2022-08-31 21:53:01 -0500

    Complete Palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,36 @@
-const palindromes = function () {
+const palindromes = function (testString) {
+  let testArray = testString.toLowerCase().split('')
+    , newArray = []
+    , offset = 0
+    , isPalindrome = true;
+  
+  for (let i = 0; i < testArray.length; i++) {
+//    console.log(testArray[i]);
+    switch (testArray[i]) {
+      case '!': 
+        break;
+      case '.':
+        break;
+      case ',':
+        break;
+      case ' ':
+        break;
+      default:
+        newArray.push(testArray[i]);
+    }
+  }
+  
+  for (i = 0; i < newArray.length; i++) {
+    if (newArray[newArray.length - 1 - i] !== newArray[i]) {
+      isPalindrome = false;
+    }
+  }
 
+//  console.log({testArray});
+  console.log({newArray});
+  console.log(offset)
+
+  return isPalindrome;
 };
 
 // Do not edit below this line
