420f8c7ef9dff204e534d4e833104f7e5e368145
Patrick Davey <ghent12@gmail.com>
2022-09-01 18:05:48 -0500

    Completed exercise 10, fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+  let fibonacciArray = [Number(x)];
+  fibonacciArray[0] = 1;
+  fibonacciArray[1] = 1;
 
+  for (let i = 2; i <= x; i++) {
+    fibonacciArray[i] = fibonacciArray[i-1] + fibonacciArray[i-2]
+  }
+
+  if (x < 0) {
+    return "OOPS";
+  } else {
+    return fibonacciArray[x-1];
+  }
 };
 
 // Do not edit below this line
