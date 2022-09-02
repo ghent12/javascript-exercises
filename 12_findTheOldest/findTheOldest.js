7fbf7d066491eea711aa8c246e16bb564db71afd
Patrick Davey <ghent12@gmail.com>
2022-09-02 10:19:21 -0500

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const oldest = people.sort((a, b) => {
+    let aDeath, bDeath;
+    switch (typeof a.yearOfDeath) {
+      case 'undefined':
+        aDeath = new Date().getFullYear();
+        break;
+      default:
+        aDeath = a.yearOfDeath;
+    }
+    switch (typeof b.yearOfDeath) {
+      case 'undefined':
+        bDeath = new Date().getFullYear();
+        break;
+      default:
+        bDeath = b.yearOfDeath;
+    }
 
+
+    const lastGuy = aDeath - a.yearOfBirth;
+    const nextGuy = bDeath - b.yearOfBirth;
+    return lastGuy > nextGuy ? -1 : 1;
+  })
+  console.log(oldest);
+  return oldest[0];
 };
 
 // Do not edit below this line
